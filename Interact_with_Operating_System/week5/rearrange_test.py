0eeead60669ff600f511cd1fc2a775664badbf0c
agron911 <agron0911@gmail.com>
2021-06-29 12:49:43 +0800

    week4&5

---
+++ Interact_with_Operating_System/week5/rearrange_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from rearrange import rearrange_name
+import unittest
+
+class TestRearrange(unittest.TestCase):
+    def test_basic(self):
+        testcase = 'love, ada'
+        expected = 'ada love'
+        self.assertEqual(rearrange_name(testcase),expected)
+    def test_empty(self):
+        testcase=''
+        expected=''
+        self.assertEqual(rearrange_name(testcase),expected)
+unittest.main()
